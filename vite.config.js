96ca60c3e06bce9cc0017ac63c5b850bcd282dd3
Amine El Ghayate <aelghayate@simplonprod.co>
2021-10-01 02:08:05 +0200

    🏋🏼‍♂️

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ const path = require('path');
 export default {
     alias: {
         kaboom: path.resolve(__dirname, 'node_modules/kaboom/dist/kaboom.mjs'),
-        base: '/spong/'
+        base: './'
   }
 }
